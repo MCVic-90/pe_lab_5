d484b2ebab9b46fe155bda7da4b07957d5921c90
Victor_Kochukov <victorkochukow90@gmail.com>
2020-12-24 21:13:49 +0300

    creating dictionary and pickle file only with 'ё' words

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import nltk
+import pickle
+
+path = 'C:\\Users\\MC_VIC\\Dropbox\\1 семестр\\ПИ\\лр4\\dictionary.pkl'
 
 
 def main():
@@ -11,21 +15,37 @@ def main():
     headers = soup.find_all('p')
 
     word_list = []
+
     for wrd in headers[38:]:
         word_list.append(wrd.text)
 
     active_list = []
     for el in word_list:
         if len(el) >= 2:
-            active_word = el.replace('.', ' ').replace(',', ' ').split()
-            if len(active_word[0]) >= 2:
-                first_wrd = active_word[0]
-                active_list.append(first_wrd)
+            el.replace('.', '').replace(',', '')
+            active_list.append(el[0:])
 
-    #for wrd in enumerate(active_list):
-    #   print(str(wrd[0]) + " -- " + str(wrd[1]))
+    active_list[2] = active_list[2].replace('n', ' ').split().pop(0)
 
-    print(str(len(active_list)-1) + " words in dictionary")
+    alone_wrds = nltk.word_tokenize(str(active_list), "russian")
+
+    goal_wrds = [x for x in alone_wrds if list(x).__contains__('ё')]
+
+    only_wrds = []
+
+    for i in goal_wrds:
+        if not i[0].isalpha():
+            only_wrds.append(i[1:])
+        elif i[0].isalpha():
+            only_wrds.append(i)
+
+    pickle_create_file(only_wrds)
+
+
+def pickle_create_file(list):
+    f = open(path, 'wb')
+    pickle.dump(list, f)
+    print("Словарь в файле PICKLE и сохранен в " + path + "!")
 
 
 if __name__ == '__main__':
